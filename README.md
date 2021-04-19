d031384bc134b66e09f56ec4864883df61ac2ba5
eualexdev <eujosealexsandro@gmail.com>
2021-04-19 15:24:47 -0300

    Corigir mais alguns bugs

---
+++ README.md
@@ -0,0 +1,1 @@
+# Api para Elegant Ide
---
+++ fileDawnloads.js
@@ -0,0 +1,7 @@
+const json = {
+    "files":[
+        "https://elegant-ide-api.netlify.app/img/ConfigureIcon.png"
+    ]
+}
+console.log(json)
+document.body.innerHTML = JSON.stringify(json)
